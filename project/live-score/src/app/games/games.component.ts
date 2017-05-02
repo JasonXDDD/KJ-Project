62aecd3c006df338a649ad49ac7559e43647d121
Len_AshBell <jason1022liu@gmail.com>
2017-05-02 12:39:46 +0800

    update livescore plugins

--- project/live-score/src/app/games/games.component.ts
+++ project/live-score/src/app/games/games.component.ts
@@ -10,6 +10,29 @@ export class GamesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.doShowScore();
+  }
+
+  doShowScore() {
+
+    var minimalData = {
+      teams: [
+        ["Team 1", "Team 2"], /* first matchup */
+        ["Team 3", "Team 4"],  /* second matchup */
+        ["Team 12", "Team 12"], /* first matchup */
+        ["Team 13", "Team 14"]  /* second matchup */
+      ],
+      results: [
+        [[22, 12], [3, 45]],       /* first round */
+        [[3, 44], [2, 1]]        /* second round */
+      ]
+    }
+
+    $(function () {
+      $('#score').bracket({
+        init: minimalData /* data to initialize the bracket with */
+      })
+    })
   }
 
 }
