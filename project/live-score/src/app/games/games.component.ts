a68388d256c2aac6b0f7c10776fdd610e8e96800
Len_AshBell <jason1022liu@gmail.com>
2017-05-10 15:33:24 +0800

    update sockit io update view, XDDDDDDDDDDDDDDDDDDD

--- project/live-score/src/app/games/games.component.ts
+++ project/live-score/src/app/games/games.component.ts
@@ -13,11 +13,11 @@ import {Observable} from 'rxjs/Observable';
 export class GamesComponent implements OnInit {
   typeItem: any;
   roundrobin: any;
+  isStart: boolean;
   constructor(private http: Http, private datePipe: DatePipe, private zone: NgZone) {
     this.roundrobin = {
       class: [],
-      data: {},
-      games: []
+      data: {}
     }
   }
 
@@ -25,26 +25,8 @@ export class GamesComponent implements OnInit {
 
     this.typeItem = JSON.parse(sessionStorage.getItem('typeItem'))
     this.doGetGames();
-
-    Observable.create(observer =>{
-      var socket = io('http://huangserver.ddns.net:3030/');
-      var app = feathers()
-        .configure(feathers.hooks())
-        .configure(feathers.socketio(socket));
-      //When games update
-      socket.on('patched', (message) => {
-        console.log('Someone patched a message', message);
-        observer.next(message)
-      })
-    })
-
-    .subscribe(message => {
-      this.zone.run(()=>{
-        this.roundrobin.games.forEach(element => {
-          if(element.game_id === message.game_id) element = message;
-        });
-      })
-    })
+    this.initSocket()
+    
 
     // this.doShowKnockoutScore();
 
@@ -55,17 +37,32 @@ export class GamesComponent implements OnInit {
       .subscribe(result =>{
         this.roundrobin.class = Object.keys(result.json());
         this.roundrobin.data = result.json();
-        this.roundrobin.class.forEach(element=>{
-          this.roundrobin.data[element].games.forEach(game => {
-            this.roundrobin.games.push(game);
-          });
-        })
-
-        console.log(this.roundrobin.games)
       })
   }
-  doShowRoundRobinScore(){
 
+  checkIsStarting(gameClass, idRight, idTop){
+    var type;
+    this.roundrobin.data[gameClass].games.forEach(element=>{
+      if(element.team_A_id ===  idRight && element.team_B_id === idTop){
+        if (element.game_status === "preparing") type = 3;
+        else if(element.game_status === "finished") type = 4;
+        else type = 1;
+      }
+      else if(element.team_A_id === idTop && element.team_B_id ===  idRight){
+        if (element.game_status === "preparing") type = 3;
+        else if(element.game_status === "finished") type = 4;
+        else type = 2;
+      }
+      else if(element.team_A_id === element.team_B_id){
+        type = 0;
+      }
+    });
+
+    if(type === 1) return true;
+    else if(type === 2) return true;
+    else if(type === 3) return false;
+    else if(type === 4) return false;
+    else return false;
   }
 
   calcScore(gameClass, idRight, idTop){
@@ -75,11 +72,13 @@ export class GamesComponent implements OnInit {
       if(element.team_A_id ===  idRight && element.team_B_id === idTop){
         game = element;
         if (element.game_status === "preparing") type = 3;
+        else if(element.game_status === "finished") type = 4;
         else type = 1;
       }
       else if(element.team_A_id === idTop && element.team_B_id ===  idRight){
         game = element;
         if (element.game_status === "preparing") type = 3;
+        else if(element.game_status === "finished") type = 5;
         else type = 2;
       }
       else if(element.team_A_id === element.team_B_id){
@@ -87,15 +86,34 @@ export class GamesComponent implements OnInit {
       }
     });
 
-    if(type === 1) return game.team_A_score + ":" + game.team_B_score;
-    else if(type === 2) return game.team_B_score + ":" + game.team_A_score;
+    if(type === 1) return game.team_A_score + ":" + game.team_B_score + "\n進行中";
+    else if(type === 2) return game.team_B_score + ":" + game.team_A_score + "\n進行中";
     else if(type === 3) return "[" + game.game_place + "]\n" + this.datePipe.transform(game.game_date, 'yyyy.MM.dd') + "\n" + game.game_time;
+    else if(type === 4) return game.team_A_score + ":" + game.team_B_score + "\n已結束";
+    else if(type === 5) return game.team_B_score + ":" + game.team_A_score + "\n已結束";
     else return "＼\n　＼\n　　＼";
   }
 
 
-  initSocket(observer) {
+  initSocket() {
+      var socket = io('http://huangserver.ddns.net:3030/');
+      var app = feathers()
+        .configure(feathers.hooks())
+        .configure(feathers.socketio(socket));
+      //When games update
+      socket.on('patched', (message) => {
+        console.log('Someone patched a message', message);
+        this.roundrobin.class.forEach(element => {
+          this.roundrobin.data[element].games.forEach(function(ele,index,array){
+            if(ele.game_id === message.game_id)
+              array[index] = message;
+          })
+        });
+        
+        
+      })
 
+      console.log(this.roundrobin)
   }
 
 
